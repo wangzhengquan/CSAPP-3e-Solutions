f271f605b82b3135ac91ef462eecfca9389ca0b5
wangzhengquan <wangzhengquan85@126.com>
2019-10-11 23:03:31 +0800

    update convert.c

--- chapter6/code/convert.c
+++ chapter6/code/convert.c
@@ -10,12 +10,12 @@
 #include <time.h>
 
 // a large prime number
-#define MATRIX_N 9973
+#define MATRIX_N 5000
 #define MEM_SIZE (sizeof(int) * MATRIX_N * MATRIX_N)
-#define LOOP 1000
-#define BLOCK 16
+#define BLOCK 8
 
-void randomize(int *arr, int N) {
+void randomize(int *arr, int N)
+{
   srand(time(0));
 
   int i, j;
@@ -24,7 +24,8 @@ void randomize(int *arr, int N) {
       arr[i * N + j] = rand() % 2;
 }
 
-void convert(int *src, int N) {
+void convert(int *src, int N)
+{
   int i, j;
 
   for (i = 0; i <= N - 1; i++)
@@ -32,77 +33,99 @@ void convert(int *src, int N) {
       src[j * N + i] = src[i * N + j] || src[j * N + i];
 }
 
-void effective_convert(int *src, int N) {
-  int i, j, a, b, tmp;
-
-  for (i = 0; i <= N - BLOCK; i += BLOCK)
-    /* not j = 0 here */
-    for (j = i; j <= N - BLOCK; j += BLOCK)
-      for (a = i; a < i + BLOCK; a++)
-        for (b = j; b < j + BLOCK; b++) {
-          /* brilliant! store two value in one loop */
-          tmp = src[b * N + a] || src[a * N + b];
-          src[b * N + a] = tmp;
-          src[a * N + b] = tmp;
-        }
-
-
-  int offset = i;
-
-  for (i = offset; i <= N - 1; i++)
-    for (j = 0; j < offset; j += BLOCK)
-      for (b = j; b < j + BLOCK; b++) {
-        tmp = src[b * N + i] || src[i * N + b];
-        src[b * N + i] = tmp;
-        src[i * N + b] = tmp;
-      }
-
-  for (i = offset; i <= N - 1; i++)
-    for (j = i; j <= N - 1; j++) {
-      tmp = src[j * N + i] || src[i * N + j];
-      src[j * N + i] = tmp;
-      src[i * N + j] = tmp;
+void effect_convert1(int *G, int dim)
+{
+  int i, j;
+  int val;
+  for (i = 0; i < dim; i++)
+    for (j = 0; j < i; j++)
+    {
+      val = G[i * dim + j] || G[j * dim + i];
+      G[i * dim + j] = val;
+      G[j * dim + i] = val;
     }
 }
 
-void test(void) {
+void effect_convert2(int *G, int dim)
+{
+  int i , j, a, b;
+  int val;
+  for (i = dim - 1; i >= BLOCK; i -= BLOCK)
+  {
+    for (j = i; j >= BLOCK; j -= BLOCK)
+    {
+      for (a = i; a > i - BLOCK; a--)
+        for (b = j; b > j - BLOCK; b--)
+        {
+          val = G[a * dim + b] || G[b * dim + a];
+          G[a * dim + b] = val;
+          G[b * dim + a] = val;
+        }
+    }
+    for (; j >= 0; j--)
+    {
+      for (a = i; a > i - BLOCK; a--)
+      {
+        val = G[a * dim + j] || G[j * dim + a];
+        G[a * dim + j] = val;
+        G[j * dim + a] = val;
+      }
+    }
+  }
+
+  for (; i >= 0; i--)
+  {
+    for (j = i; j >= BLOCK; j -= BLOCK)
+    {
+      for (b = j; b > j - BLOCK; b--)
+      {
+        val = G[i * dim + b] || G[b * dim + i];
+        G[i * dim + b] = val;
+        G[b * dim + i] = val;
+      }
+    }
+    for (; j >= 0; j--)
+    {
+      val = G[i * dim + j] || G[j * dim + i];
+      G[i * dim + j] = val;
+      G[j * dim + i] = val;
+    }
+
+  }
+
+}
+
+
+
+void test(void)
+{
   int *s = (int *)malloc(MEM_SIZE);
   int *e = (int *)malloc(MEM_SIZE);
+  int *e2 = (int *)malloc(MEM_SIZE);
 
   randomize(s, MATRIX_N);
   memcpy(e, s, MEM_SIZE);
+  memcpy(e2, s, MEM_SIZE);
 
 
   convert(s, MATRIX_N);
-  effective_convert(e, MATRIX_N);
+  effect_convert1(e, MATRIX_N);
+  effect_convert2(e2, MATRIX_N);
 
   for (int i = 0; i < MATRIX_N; i++)
     for (int j = 0; j < MATRIX_N; j++)
-      assert(s[i * MATRIX_N + j] == e[i * MATRIX_N + j]);
+    {
+      assert(s[i * MATRIX_N + j] == e[i * MATRIX_N + j] );
+      assert(s[i * MATRIX_N + j] == e2[i * MATRIX_N + j] );
+    }
 
   free((void *)s);
   free((void *)e);
+  free((void *)e2);
 }
 
-void prof(void) {
-  int *s = (int *)malloc(MEM_SIZE);
-
-  for (int c = 0; c < LOOP; c++)
-    convert(s, MATRIX_N);
-
-  free((void *)s);
-}
-
-void prof_effect(void) {
-  int *e = (int *)malloc(MEM_SIZE);
-
-  for (int c = 0; c < LOOP; c++)
-    effective_convert(e, MATRIX_N);
-
-  free((void *)e);
-}
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   test();
 
   /* prof(); */

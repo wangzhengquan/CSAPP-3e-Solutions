98674e41f33aad40a53f4331a0f718f6f7d81c93
jimi-wzq <wangzhengquan85@gmail.com>
2020-01-01 20:05:17 +0800

    updat

--- chapter10/code/10.10.c
+++ chapter10/code/10.10.c
@@ -1,26 +1,27 @@
 /*
  * 10.10.c
  */
-#include <stdio.h>
 #include "csapp.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char **argv)
+{
   int n;
   rio_t rio;
   char buf[MAXLINE];
-
+  int fd = -1;
   if (argc == 2) {
-    int fd = Open(argv[1], O_RDONLY, 0);
-    while ((n = Rio_readn(fd, buf, MAXBUF)) != 0)
-      Rio_writen(STDOUT_FILENO, buf, n);
-    exit(0);
+    fd = Open(argv[1], O_RDONLY, 0);
+    Dup2(fd, STDIN_FILENO);
   }
 
   Rio_readinitb(&rio, STDIN_FILENO);
   while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0)
     Rio_writen(STDOUT_FILENO, buf, n);
-
-  return 0;
+  if (fd != -1)
+    close(fd);
+  exit(0);
 }
+
+
 
 
